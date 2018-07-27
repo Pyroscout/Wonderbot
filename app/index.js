91b18c0eeea80851693a72c5304ca4715cfc4c18
Greg Rosich <gregrosich@Gregs-MacBook-Pro.local>
2018-07-27 16:05:05 -0600

    Can join and leave voice

---
+++ app/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === '/join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
+
+  if(message.content == '/leave') {
+    client.voiceConnections.get(message.guild.id).channel.leave()
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
